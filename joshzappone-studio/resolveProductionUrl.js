6ceab2333de1bc3285781ff7c3fa4e700088397e
Josh Zappone <zappone.josh@gmail.com>
2021-02-22 17:06:30 -0800

    update production url for studio preview

--- joshzappone-studio/resolveProductionUrl.js
+++ joshzappone-studio/resolveProductionUrl.js
@@ -1,5 +1,5 @@
 const previewSecret = 'secret_time' // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000'
+const projectUrl = 'https://www.joshzappone.tech'
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
